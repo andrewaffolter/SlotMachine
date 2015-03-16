833783948827541f878f9dbb57e72ec94fdb9900
Andrew Affolter <andrewaffolter@gmail.com>
2015-03-15 22:11:52 -0700

    Commit the game

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Andrew Affolter on 3/14/15.
+//  Copyright (c) 2015 andrew. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
